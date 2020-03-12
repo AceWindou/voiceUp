09a138d4daa2ebf6ad9587904faa91f9b0565b01
Nicolas Taylor <nicolas.taylor@gmx.net>
2020-03-12 11:41:56 +0100

    debug

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.render('index', { title: 'Eugen<3' });
-=======
-  res.render('index', { title: 'BulletForce' });
->>>>>>> fc0512b2e775f5dd4e4b41166d2b8683bd4f5b3d
 });
 
 module.exports = router;
