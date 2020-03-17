a03701cef33f2eb987fb11f555d37884fdec0844
attahualpa <61975270+attahualpa@users.noreply.github.com>
2020-03-17 17:30:02 +0100

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,23 @@
 //Author Alexander Kuemmel
 //17.03.2020 Prototyp
-
-//Dieses Script catched url anfragen von Alexa und leitet diese an die Datenbank weiter
-//Changed = 1/0 wird in frontend.js abgefragt
-//res.send = return Statement der Anfrage
-
+/*
+Dieses Script catched url anfragen von Alexa und leitet diese an die Datenbank weiter
+Changed = 1/0 wird in frontend.js abgefragt
+res.send = return Statement der Anfrage
+*/
 var express = require('express');
+
 var router = express.Router();
 
 var ADRESSE;
 var CHANGED = "0";
 
 router.get('/name*', function(req, res, next) {
-	//Gibt den Request-Pfad zurueck
-	res.send(router.stack[0].path);
-});
-
-router.get('/karim', function(req, res, next) {
 	CHANGED = "1";
-	ADRESSE = "https://www.hs-fulda.de/angewandte-informatik/ueber-uns/professuren/details/person/prof-dr-karim-khakzar-307/contactBox";
-	res.send("ok: karim");
-});
-
-router.get('/kathrin', function(req, res, next) {
-	CHANGED = "1";
-	ADRESSE = "https://www.hs-fulda.de/sozialwesen/ueber-uns/professuren/details/person/prof-dr-kathrin-becker-schwarze-17/contactBox";
-	res.send("ok: kathrin");
+	ADRESSE = "";
+	//var str = router.stack[0].path; //holt sich die Request-URL
+	
+	res.send("ok");
 });
 
 router.get('/adresse', function(req, res, next) {
@@ -37,5 +29,6 @@ router.get('/changed', function(req, res, next) {
 	console.log(CHANGED);
 	res.send(CHANGED);
 });
+
 
 module.exports = router;
